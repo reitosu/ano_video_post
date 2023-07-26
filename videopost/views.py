5fe3a6ca2f513fdca38b32fd1abbccea16b63052
reitosu <134916378+reitosu@users.noreply.github.com>
2023-07-26 20:26:15 +0900

    エディット画面の基本機能の８割実装

--- videopost/views.py
+++ videopost/views.py
@@ -1,13 +1,30 @@
+from django.conf import settings
 from django.shortcuts import render,redirect
 from django.views.generic import TemplateView
-from django.http import HttpResponse,JsonResponse
+from django.http import JsonResponse
 from django.urls import reverse
-from django.core.paginator import EmptyPage, PageNotAnInteger
-from .models import Video
+from django.core.signals import request_finished
+from django.core.files.base import ContentFile
+from django.core.files.storage import FileSystemStorage
+from django.db import connection
 from django.views.generic import CreateView,ListView
+from .models import Video
 from .forms import VideoForm
+import os
+import base64
+import moviepy.editor
+import uuid
+import shutil
 
 # Create your views here.
+def create_unique_id(request):
+        unique_id = uuid.uuid4()
+        request.session['user_id'] = str(unique_id)
+
+def unique_id_assignment(request):
+    if 'user_id' not in request.session:
+        create_unique_id(request)
+    return redirect("index")
 
 class IndexView(ListView):
     model = Video
@@ -34,7 +51,7 @@ class EditView(CreateView):
     model = Video
     form_class = VideoForm
     template_name = 'edit.html'
-    
+
     def get_context_data(self, **kwargs):
         context = super(EditView, self).get_context_data(**kwargs)
         context["title"] = "エディット"
@@ -51,6 +68,69 @@ class CheckView(TemplateView):
         context['videos'] = Video.objects.all().order_by('-uploaded')
         context['title'] = "チェック"
         return context
+
+class VideoView(TemplateView):
+    template_name = 'video.html'
     
-def index(request):
-    return HttpResponse("Hello World")
+def video_post(request):
+    if request.method == 'POST':
+        video_file = request.FILES.get('videoData')  # 動画ファイルを取得
+        print(request.POST)
+        print(request.FILES)
+        print(video_file)
+        print(type(settings.STATIC_ROOT))
+        if video_file:
+            if 'user_id' not in request.session:
+                create_unique_id(request)
+            id = request.session.get('user_id')
+            print(id)
+            print(type(id))
+            path = os.path.join(settings.STATIC_ROOT,'materials',id)
+            fs = FileSystemStorage(location=path)
+            fs.save(video_file.name,video_file)
+            path = fs.path(video_file.name)
+            if 'starttime' in list(request.POST):
+                start_time = request.POST.get('starttime')
+                end_time = request.POST.get('endtime')
+                video = moviepy.editor.VideoFileClip(path)
+                clipped_video = video.subclip(start_time, end_time)
+                clipped_video.write_videofile(path)
+            path = "\\"+os.path.join(*path.split('\\')[3:])
+        return JsonResponse({'path':path})
+                    
+def image_post(request):
+    if request.method == 'POST':
+        image_data = request.POST.get('photo')
+        format, imgstr = image_data.split(';base64,')
+        ext = format.split('/')[-1]
+        image_data = base64.b64decode(imgstr)
+        file = ContentFile(image_data)
+        name = request.POST.get('name')+'.'+ext
+        id = request.session.get('user_id')
+        path = os.path.join(settings.STATIC_ROOT,'materials',id)
+        fs = FileSystemStorage(location=path)
+        fs.save(name,file)
+        path = fs.path(name)
+        path = "\\"+os.path.join(*path.split('\\')[3:])
+
+        
+        return JsonResponse({'path':path})
+
+def delete_materials(request):
+    id = request.session.get('user_id')
+    path = os.path.join(settings.STATIC_ROOT,'materials',id)
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    print("deleted")
+    return redirect('/videopost/edit')
+
+def save_backup(request):
+    print("r")
+    if request.method == "POST":
+        print(request.POST)
+        print(request.POST.get("backup_materials"))
+        print(request.POST.get("backup_previews"))
+        request.session["backup_materials"] = request.POST.get("backup_materials")
+        request.session["backup_previews"] = request.POST.get("backup_previews")
+        request.session["backup_width"] = request.POST.get("backup_width")
+        return JsonResponse({"backup": "success"})
